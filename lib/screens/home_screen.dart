2861a59a93412dc007bb773878ea85fff6af6da5
Ali Shan Aziz <jonah2114@gmail.com>
2023-01-27 04:29:18 +0500

    Transactions added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/transactions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Transactions> transactions = [
+    Transactions(
+        id: '1', title: 'New Laptop', amount: 90.99, date: DateTime.now()),
+    Transactions(
+        id: '2', title: 'New Clothes', amount: 40.56, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +31,50 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Text('CHART!'),
             ),
           ),
-          const Card(
-            child: Text('LIST OF TX!'),
+          Column(
+            children: transactions.map((trans) {
+              return Card(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        trans.amount.toString(),
+                        style: const TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          trans.title,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          trans.date.toString(),
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
